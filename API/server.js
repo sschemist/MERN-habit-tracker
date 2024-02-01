22dfd2d19b595806198fdadd403e4c96961b95b7
Sathun Suthakaran <81481873+sschemist@users.noreply.github.com>
2024-02-01 08:36:58 -0500

    Update and rename index.js to server.js

---
+++ API/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app  = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Connected to DB")).catch(console.log("whoopsie"));
+
+const Todo = require('./model/Todo');
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+
+    res.json(todos);
+})
+
+app.listen(3001, () => console.log("Server started on port 3001"));
