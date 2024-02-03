fcbf26ffa5310c92581e374d8e93c570e6f67255
sschemist <Sathun@gmail.com>
2024-02-03 15:28:00 -0500

    added react and added more CRUD functionality

--- API/server.js
+++ API/server.js
@@ -2,26 +2,45 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app  = express();
+const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("Connected to DB")).catch(console.log("whoopsie"));
+mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+}).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
-const Todo = require('./model/Todo');
+// Models
+const Todo = require('./models/Todo');
+
 app.get('/todos', async (req, res) => {
-    const todos = await Todo.find();
+	const todos = await Todo.find();
 
-    res.json(todos);
-})
+	res.json(todos);
+}); 
 
-app.get('/todo/new', async (req, res) => {
-    const todos = await Todo.find();
-    text: req.body.text
-    res.json(todos);
-})
+app.post('/todos/new', (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	})
+
+	todo.save();
+
+	res.json(todo);
+});
+
+app.delete('/todo/delete/:id', async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+    res.json(result);
+});
+
+app.put('/todo/complete/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+    todo.complete = !todo.complete;
+    todo.save();
+    res.json(todo);
+});
 
 app.listen(3001, () => console.log("Server started on port 3001"));
---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+const API_BASE = "http://localhost:3001";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+  useEffect(() => {
+    GetTodos();
+    console.log(todos);
+  }, [])
+  const GetTodos = () => {
+    fetch(API_BASE + "/todos")
+    .then(res => res.json())
+    .then(data => setTodos(data))
+    .catch(err => console.error("Error: ", err));
+  }
+  const completeTodo = async id => {
+    const data = fetch(API_BASE + '/todo/complete/' + id)
+    .then(res => res.json());
+    setTodos(todos => todos.map(todo => {
+      if (todo._id === data._id) {
+        todo.complete = data.complete;
+      }
+      return todo;
+    }))
+  }
+
+  return (
+    <div className="App">
+        <h1>Welcome, Sathun</h1>
+        <h1>Your habits</h1>
+        <div className="todos">
+          {todos.map(todo => (
+          <div className={"todo " + (todo.complete ? "is-complete" : "")} key = {todo._id} onClick={() => completeTodo(todo._id)}>
+            <div className="checkbox"></div>
+            <div className="text">{todo.text}</div>
+            <div className="delete-todo">x</div>
+          </div>
+          ))}
+        </div>
+    </div>
+  );
+}
+
+export default App;
