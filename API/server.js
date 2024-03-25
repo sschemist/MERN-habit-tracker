7a4e81cd468d012d04fe08f8bdaefcd201cceb24
sschemist <Sathun@gmail.com>
2024-03-24 22:44:04 -0400

    updated with all CRUD functionality

--- API/server.js
+++ API/server.js
@@ -19,9 +19,9 @@ app.get('/todos', async (req, res) => {
 	const todos = await Todo.find();
 
 	res.json(todos);
-}); 
+});
 
-app.post('/todos/new', (req, res) => {
+app.post('/todo/new', (req, res) => {
 	const todo = new Todo({
 		text: req.body.text
 	})
@@ -32,15 +32,29 @@ app.post('/todos/new', (req, res) => {
 });
 
 app.delete('/todo/delete/:id', async (req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id);
-    res.json(result);
+	const result = await Todo.findByIdAndDelete(req.params.id);
+
+	res.json({result});
 });
 
 app.get('/todo/complete/:id', async (req, res) => {
-    const todo = await Todo.findById(req.params.id);
-    todo.complete = !todo.complete;
-    todo.save();
-    res.json(todo);
+	const todo = await Todo.findById(req.params.id);
+
+	todo.complete = !todo.complete;
+
+	todo.save();
+
+	res.json(todo);
+})
+
+app.put('/todo/update/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.text = req.body.text;
+
+	todo.save();
+
+	res.json(todo);
 });
 
-app.listen(3001, () => console.log("Server started on port 3001"));
+app.listen(3001);
